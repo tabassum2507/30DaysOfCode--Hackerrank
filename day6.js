d0703bf0f6fe7a94620665edaafbb1b5a6dcdf56
tabassum2507 <10tabassum2@gmail.com>
2022-09-14 17:09:43 +0530

    d-6

---
+++ day6.js
@@ -0,0 +1,25 @@
+// Day 6: Let's Review
+
+function processData(input) {
+    //Enter your code here
+    
+     input = input.split('\n')
+     
+    for(let i=1 ; i<input.length; i++){
+        let splitStr = input[i].split('')
+        // console.log(splitStr);
+    
+    let even = '';
+    let odd = '';
+    
+    for(let x = 0; x< splitStr.length; x++){
+        if( x%2 == 0){
+            even = even + splitStr[x]
+        }
+        else {
+            odd = odd + splitStr[x]
+        }
+    }
+    console.log(even + ' ' + odd)
+    }
+} 
